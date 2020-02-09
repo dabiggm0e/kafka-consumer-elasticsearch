d301a1c05b4e54580e6f66cf3e755a67870caeb7
moe <Mo@homebase>
2020-02-09 17:58:54 +0300

    Now kafka consumer is writing into elasticsearch

---
+++ src/main/java/com/github/dabiggm0e/kafka/KafkaConsumerClient.java
@@ -0,0 +1,66 @@
+package com.github.dabiggm0e.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+
+
+public class KafkaConsumerClient {
+    static Logger logger = LoggerFactory.getLogger(KafkaConsumerClient.class.getName());
+    String bootstrapServers;
+    String groupId;
+    String topic;
+
+    public KafkaConsumerClient( String bootstrapServers,
+                                String groupId,
+                                String topic) {
+        this.bootstrapServers = bootstrapServers;
+        this.groupId = groupId;
+        this.topic = topic;
+
+    }
+
+
+    public KafkaConsumer<String, String> createConsumer() {
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        //subscribe to topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        return consumer;
+    }
+
+    public static void main(String[] args) {
+
+/*
+
+        // poll data
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(duration));
+            for (ConsumerRecord<String, String> record: records) {
+                logger.info("Topic: " + record.topic() + ", Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+
+*/
+    }
+}
--- src/main/java/com/github/dabiggm0e/kafka/elasticsearch/ElasticSearchConsumer.java
+++ src/main/java/com/github/dabiggm0e/kafka/elasticsearch/ElasticSearchConsumer.java
@@ -1,29 +1,82 @@
 package com.github.dabiggm0e.kafka.elasticsearch;
 
+import com.github.dabiggm0e.kafka.KafkaConsumerClient;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.client.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
 
 public class ElasticSearchConsumer {
 
     private static JsonParser jsonParser = new JsonParser();
 
+    static Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+
     public static String extractIdFromTweet(String jsonRequest) {
         String id = "";
-       // jsonParser.parse(jsonRequest)
-         //       .getAsString().
+     /*   jsonParser.parse(jsonRequest)
+                .getAsString().*/
         return id;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String bootstrapServers = "localhost:9092";
+        String groupId = "my-fifth-application";
+        String topic = "twitter-tweets";
+        int duration = 100;
+
+        KafkaConsumerClient kafkaConsumerClient = new KafkaConsumerClient(
+                bootstrapServers,
+                groupId,
+                topic
+        );
+
+        KafkaConsumer<String, String> kafkaConsumer = kafkaConsumerClient.createConsumer();
+
+
         String hostname = "localhost";
         RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 9200, "http"));
         RestHighLevelClient client = new RestHighLevelClient(builder);
 
         String jsonString = "{ \"foo\": \"bar\" }";
+
+        // poll data
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(duration));
+            for (ConsumerRecord<String, String> record: records) {
+                logger.info("Topic: " + record.topic() + ", Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter",
+                        "tweets"
+                ).source(record.value(), XContentType.JSON);
+
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+
+                logger.info(id);
+
+            }
+        }
+
+        //client.close();
+
     }
 }
